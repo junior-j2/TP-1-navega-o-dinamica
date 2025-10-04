5dcdbd0ccd66498bb1998ce65d7eeda8b0ad6fdd
Divan junior <juninho.antunessene@hotmail.com>
2025-10-04 13:32:23 +0000

    update

---
+++ assets/js/bd.js
@@ -0,0 +1,32 @@
+const dados = [
+    {
+        "id": 0,
+        "titulo": "Transporte Público em Belo Horizonte",
+        "descricao": "Tarifa zero é uma política de gratuidade no transporte coletivo. É a proposta de que as pessoas não paguem na hora de utilizar serviços de ônibus ou metrô. Simples, né?",
+        "conteudo": "A tarifa zero é uma realidade em mais de 150 cidades do mundo. E pode acontecer em Belo Horizonte também! No Brasil, a maior cidade com tarifa zero é Maricá, no Rio de Janeiro, com cerca de 160 mil habitantes. Mais próximo a Belo Horizonte, temos Itatiaiuçu, na Região Metropolitana de BH. Podemos aprender com essas experiências e pensar em formas de tornar a tarifa zero realidade no nosso contexto.",
+        "categoria": "Transporte",
+        "autor": "Ana Clara Pereira",
+        "data": "26/09/2025",
+        "imagem": "./assets/imagem/transporte-publico.png"
+    },
+    {
+        "id": 1,
+        "titulo": "Tecnologia 6G",
+        "descricao": "A Tecnologia 6G é a Próxima Geração de Redes Móveis",
+        "conteudo": "A tecnologia 6G é a próxima geração de redes móveis, ainda em desenvolvimento, que sucederá o 5G. Prevista para ser lançada comercialmente por volta de 2030, a 6G promete uma revolução na conectividade, com velocidades muito superiores, latência quase instantânea e integração massiva de inteligência artificial (IA).",
+        "categoria": "Tecnologia",
+        "autor": "Bruno Costa e Silva",
+        "data": "25/09/2025",
+        "imagem": "./assets/imagem/tecnologia-6g.png"
+    },
+    {
+        "id": 2,
+        "titulo": "Festival de Música de Belo Horizonte",
+        "descricao": "Belo Horizonte tem Vários Festivais de Música",
+        "conteudo": "Belo Horizonte tem vários festivais de música, como o Festival Planeta Brasil, que é o maior da cidade, e a Virada Cultural, um evento gratuito de 24 horas focado em diversidade artística. Também existem festivais temáticos, como o Épop BH para música pop autoral e o Festival Alvoroço, focado em música brasileira em formato acústico e intimista.",
+        "categoria": "Cultura",
+        "autor": "Carla Vieira Grossi",
+        "data": "24/09/2025",
+        "imagem": "./assets/imagem/festival-de-musica.png"
+    }
+];
